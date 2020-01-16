7ad72273c43176287ff8a6e18a26f84d80cbeb2b
Stephen George <sgit-turtle.testudo12@wronghead.com>
2020-01-16 07:55:28 -0600

    Fix publishing

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Stephen George',
   subHeading: '[engineering manager and techie extraordinaire]',
 
