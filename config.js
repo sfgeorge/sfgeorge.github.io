52761681c3fc9a12efdfdb30973603565b236143
Stephen George <sgit-turtle.testudo12@wronghead.com>
2020-01-16 14:52:26 -0600

    Conditionalize the showing of Email, Address and Phone

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ module.exports = {
     },
 /* .fa-linux .fa-amazon .fa-asterisk .fa-aws .fa-compass .fa-dev .fa-docker .fa-git .fa-html5 .fa-jedi .fa-jedi-order .fa-jira .fa-laptop .fa-laptop-medical .fa-mandalorian .fa-meetup  .fa-music .fa-node .fa-node-js .fa-npm .fa-phoenix-framework  .fa-phoenix-squadron .fa-phoenix-phone .fa-php .fa-poop .fa-portrait .fa-project-diagram .fa-product-hunt .fa-print .fa-raspberry-pi .fa-react  .fa-readme .fa-redhat .fa-centos .fa-reply-all .fa-sd-card .fa-share .fa-sim-card .fa-slack .fa-snowflake .fa-spa .fa-wrench */
   ],
-  email: 'anubhav.srivastava00@gmail.com',
-  phone: '000-00000',
-  address: '305 Del Sol, Sunnyvale, CA',
+  email: null,
+  phone: null,
+  address: 'Chicago, IL, USA',
 };
