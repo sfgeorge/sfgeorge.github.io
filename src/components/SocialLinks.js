b6e2536c6e0ba19883852f73f1566e54ad9472af
Stephen George <sgit-turtle.testudo12@wronghead.com>
2022-02-14 09:52:50 -0500

    More prominent LinkedIn contact

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -54,6 +54,56 @@ export default function SocialLinks() {
             : <div />
           }
 
+          {config.linkedin ?
+            <div className="col-md-4 mb-3 mb-md-0">
+              <div className="card py-4 h-100">
+                <div className="card-body text-center">
+                  <i className={`fab fa-linkedin`}></i>
+                  <h4 className="text-uppercase m-0">Contact</h4>
+                  <hr className="my-4" />
+                  <div className="small text-black-50">
+                      <i className={`fas fa-angle-right mr-1`}></i>
+                    <a target="_blank" href={`${config.linkedin}`}>
+                      {config.linkedin}
+                    </a>
+                  </div>
+                  <div className="small text-black-50">
+                      <i className={`fas fa-hand-point-right mr-1`}></i>
+                    <a target="_blank" href={`${config.linkedin}`}>
+                      {config.linkedin}
+                    </a>
+                  </div>
+                  <div className="small text-black-50">
+                      <i className={`fas fa-caret-square-right mr-1`}></i>
+                    <a target="_blank" href={`${config.linkedin}`}>
+                      {config.linkedin}
+                    </a>
+                  </div>
+                  <div className="small text-black-50">
+                      <i className={`fas fa-chevron-circle-right mr-1`}></i>
+                    <a target="_blank" href={`${config.linkedin}`}>
+                      {config.linkedin}
+                    </a>
+                  </div>
+                  <div className="small text-black-50">
+                      <i className={`fas fa-arrow-circle-right mr-1`}></i>
+                    <a target="_blank" href={`${config.linkedin}`}>
+                      {config.linkedin}
+                    </a>
+                  </div>
+                  <div className="small text-black-50">
+                      <i className={`fas fa-arrow-alt-circle-right mr-1`}></i>
+                    <a target="_blank" href={`${config.linkedin}`}>
+                      {config.linkedin}
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            : <div />
+          }
+
         </div>
 
         <div className="social d-flex justify-content-center">
