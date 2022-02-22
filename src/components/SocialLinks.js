8b916da3daaedc0f006871abe3cb8505495a6bb9
Stephen George <sgit-turtle.testudo12@wronghead.com>
2022-02-22 18:58:27 -0500

    Bigger pointer to LinkedIn

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -59,12 +59,12 @@ export default function SocialLinks() {
             <div className="col-md-4 mb-3 mb-md-0">
               <div className="card py-4 h-100">
                 <div className="card-body text-center">
-                  <i className={`fab fa-linkedin`}></i>
+                  <i className={`fab fa-linkedin text-primary mb-2`}></i>
                   <h4 className="text-uppercase m-0">Contact / Resume</h4>
                   <hr className="my-4" />
                   <div className="small text-black-50">
-                    <i className={`fas fa-hand-point-right mr-1`}></i>
                     <OutboundLink target="_blank" rel="noreferrer" href={`${config.linkedin}`}>
+                      <i className={`fas fa-hand-point-right fa-2x align-text-top mr-2`}></i>
                       {config.linkedin}
                     </OutboundLink>
                   </div>
