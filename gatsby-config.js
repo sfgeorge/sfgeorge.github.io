b77d354a62cdb0df0d1e5f91017ad29a9d8af42c
Stephen George <sgit-turtle.testudo12@wronghead.com>
2022-02-26 13:47:51 -0500

    [BREAK] incomplete adoption of gatsby-plugin-open-graph-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,5 +43,6 @@ module.exports = {
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    'gatsby-plugin-open-graph-images',
   ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,21 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const { createOpenGraphImage } = require("gatsby-plugin-open-graph-images");
+
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions;
+
+  const openGraphImage = createOpenGraphImage(createPage, {
+    path: "/og-image/index.png",
+    component: path.resolve(`src/templates/index.og-image.js`),
+    size: {
+      width: 400,
+      height: 50,
+    },
+    waitCondition: "networkidle0",
+    context: {
+      description: "a image created with gatsby-plugin-open-graph-images",
+    },
+  });
+};
